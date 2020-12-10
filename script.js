28b1b5c04d5db9e6a773ea5d496c01b4806ad4ed
root <root@DESKTOP-FRJ7R88.localdomain>
2020-12-10 06:21:16 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const questionsAndAnswers = {
+    whatIsVoldemortsName: "Tom Marvolo Riddle",
+    whatIsHedwig: "Owl",
+
+}
+    
+
+
+
+
+
+const userAnswers = [];
+
+
+const currentQuestion = 0;
+const correctAnswer = 0;
+
+// Where do I start?
+// I need an object or an array? to hold my questions and answers. I saw a way to utilize classes to organize the array list
+// I need a way to take the users input and validate it to know if it's correct or incorrect
+// I need a function that'll calculate the users score at the end, and display a win IMG or a loss IMG depending on result, should also display score percentage afterwards IF/ELSE statement
+// I can use a for loop to incrementally add/subtract from the users score i++
+// I need to create a submit button that logs the users input .onclick/.addevent?
+
+
+// are there any variables I should declare, that I know is gonna be used alot?
+// like correctAnswer, wrongAnswer, currentQuestion
+
+
